1b499c3bc71a6d3c45001fa3d1595de1336ba991
Victor Gouvêa <victorgouvea28@gmail.com>
2024-10-04 18:23:38 -0300

    Add google tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,93 +21,98 @@ export default defineNuxtConfig({
           link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
        }
    },
-  
-	compatibilityDate: "2024-08-09",
 
-	typescript: {
-		typeCheck: true,
-	},
+		compatibilityDate: "2024-08-09",
 
-	devtools: {
-		enabled: true,
-	},
+		typescript: {
+						typeCheck: true,
+		},
 
-	modules: [
-	  "@nuxt/content",
-		"@nuxt/image",
-		"@nuxtjs/tailwindcss",
-		"@primevue/nuxt-module",
-		"@sidebase/nuxt-auth",
-		"nuxt-posthog",
-	],
-	
-	content: {
-    experimental: {
+		devtools: {
+						enabled: true,
+		},
+
+		modules: [
+		 "@nuxt/content",
+		 "@nuxt/image",
+		 "@nuxtjs/tailwindcss",
+		 "@primevue/nuxt-module",
+		 "@sidebase/nuxt-auth",
+		 "nuxt-posthog",
+		 'nuxt-gtag',
+		],
+		
+		content: {
+		experimental: {
       search: {}
-    }
+		}
   },
 
-	// https://tailwind.primevue.org/nuxt/
-	// https://github.com/primefaces/primevue-examples/blob/main/nuxt-styled-tailwind/nuxt.config.ts
-	primevue: {
-		options: {
-			theme: {
-				preset: Aura,
-			},
+		// https://tailwind.primevue.org/nuxt/
+		// https://github.com/primefaces/primevue-examples/blob/main/nuxt-styled-tailwind/nuxt.config.ts
+		primevue: {
+						options: {
+										theme: {
+														preset: Aura,
+										},
+						},
 		},
-	},
 
-	// Files added here are included in all pages. We want to add the PrimeIcons
-	// CSS
-	//
-	// https://nuxt.com/docs/api/nuxt-config#css
-	// https://github.com/primefaces/primeicons
-	// https://primevue.org/icons/
-	css: ["primeicons/primeicons.css"],
-
-	auth: {
-		// This makes all pages private by default. If you want to make a page
-		// public, you can use `definePageMeta({ auth: false })` on the page `setup`
+		// Files added here are included in all pages. We want to add the PrimeIcons
+		// CSS
 		//
-		// https://auth.sidebase.io/guide/application-side/configuration#globalappmiddleware
-		globalAppMiddleware: true,
+		// https://nuxt.com/docs/api/nuxt-config#css
+		// https://github.com/primefaces/primeicons
+		// https://primevue.org/icons/
+		css: ["primeicons/primeicons.css"],
 
-		// https://auth.sidebase.io/guide/authjs/quick-start#configuration
-		provider: {
-			type: "authjs",
-			trustHost: false,
-			defaultProvider: "auth0",
-			addDefaultCallbackUrl: "/",
-		},
-	},
+		auth: {
+						// This makes all pages private by default. If you want to make a page
+						// public, you can use `definePageMeta({ auth: false })` on the page `setup`
+						//
+						// https://auth.sidebase.io/guide/application-side/configuration#globalappmiddleware
+						globalAppMiddleware: true,
 
-	// Configurations from here should be overriden by using `.env` or environment
-	// variables. Check `.env.example` for documentation on each of those values
-	//
-	// https://nuxt.com/docs/guide/going-further/runtime-config
-	runtimeConfig: {
-		secret: "",
-
-		authOrigin: "",
-
-		auth0: {
-			issuer: "",
-			clientId: "",
-			clientSecret: "",
+						// https://auth.sidebase.io/guide/authjs/quick-start#configuration
+						provider: {
+										type: "authjs",
+										trustHost: false,
+										defaultProvider: "auth0",
+										addDefaultCallbackUrl: "/",
+						},
 		},
 		
-		stripe: {
-  		secretKey: "",
-  		successUrl: "",
-  	},
+		gtag: {
+      id: process.env.NUXT_GTAG_ID,
+    },
 
-		// Configurations inside the `public` object are available to the client.
-		// Eg. in the browser
-		public: {
-			posthog: {
-			  host: process.env.NUXT_POSTHOG_HOST,
-        publicKey: process.env.NUXT_POSTHOG_PUBLIC_KEY,
-			},
+		// Configurations from here should be overriden by using `.env` or environment
+		// variables. Check `.env.example` for documentation on each of those values
+		//
+		// https://nuxt.com/docs/guide/going-further/runtime-config
+		runtimeConfig: {
+						secret: "",
+
+						authOrigin: "",
+
+						auth0: {
+										issuer: "",
+										clientId: "",
+										clientSecret: "",
+						},
+						
+						stripe: {
+						secretKey: "",
+						successUrl: "",
 		},
-	},
+
+						// Configurations inside the `public` object are available to the client.
+						// Eg. in the browser
+						public: {
+										posthog: {
+												host: process.env.NUXT_POSTHOG_HOST,
+    publicKey: process.env.NUXT_POSTHOG_PUBLIC_KEY,
+										},
+						},
+		},
 });
